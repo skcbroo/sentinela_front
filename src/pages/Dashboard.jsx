64f38096c74500b76ef68ec854ebb32ca2de4f82
skcbroo <skcbroo@gmail.com>
2025-08-06 16:17:35 -0300

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+// src/pages/Dashboard.jsx
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function Dashboard() {
+  return (
+    <AdminLayout>
+      <h1>Bem-vindo, Admin</h1>
+      {/* Conteúdo do dashboard */}
+    </AdminLayout>
+  );
+}
