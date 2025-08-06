4f2fd5b271db98040d43206f5d7579081dd6cb85
skcbroo <skcbroo@gmail.com>
2025-08-06 16:17:48 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Usuarios from "./pages/Usuarios";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Contador: {count}
-        </button>
-        <p>
-          Edite <code>src/App.jsx</code> e salve para testar o HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Clique no logo do React para saber mais
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        
+        {/* Rotas protegidas */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/usuarios"
+          element={
+            <ProtectedRoute>
+              <Usuarios />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
