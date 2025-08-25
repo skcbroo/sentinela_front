1664d63c0fb0fd93d600f7d58d14f690162fe8c3
skcbroo <skcbroo@gmail.com>
2025-08-25 09:34:29 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2F5871",       // azul profundo (marca)
+        primarydark: "#1D3557",   // azul mais escuro
+        accent: "#2B6CB0",        // para CTAs/botões
+        success: "#3C6E71",       // “ok”, confiança/cuidado
+        alert: "#DAA520",         // atenção/escalas
+        textmain: "#2D3748",      // cinza-escuro para textos
+        fog: "#F7FAFC",           // fundo claro
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
